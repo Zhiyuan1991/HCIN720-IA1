16d04fdeacdd81c59f2b6ed258fff31380d802ba
Zhiyuan Li <zl7904@g.rit.edu>
2016-09-06 17:19:04 -0400

    disp temperature
    disp temperature
    scale it to [min_max]

--- webpage.js
+++
@@ -1,7 +0,0 @@
-<html>
-	<head>
-	<meta charset="utf-8">
-	<script scr="/Users/ZhiyuanLi/Downloads/node_modules/jquery/dist/jquery.min.js"></script>
-	<script scr="/Users/ZhiyuanLi/Downloads/node_modules/paper/dist/paper-full.min.js"></script>
-	
-	</head>
